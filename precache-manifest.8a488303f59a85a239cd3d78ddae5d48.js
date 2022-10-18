e216cc66a0f5c7b11585287d7be77ea11583c92c
Yuriu Gavrikov <coolyurik2012@yandex.ru>
2022-10-18 20:31:26 +0300

    Updates

---
+++ precache-manifest.8a488303f59a85a239cd3d78ddae5d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5bfe5c7c3ada7f03f6e",
+    "url": "/react-store/static/css/main.fab939b2.chunk.css"
+  },
+  {
+    "revision": "e5bfe5c7c3ada7f03f6e",
+    "url": "/react-store/static/js/main.e5bfe5c7.chunk.js"
+  },
+  {
+    "revision": "1d7379dcc26eff5cbe55",
+    "url": "/react-store/static/js/1.1d7379dc.chunk.js"
+  },
+  {
+    "revision": "34ee57e0b9df4e434c41",
+    "url": "/react-store/static/js/runtime~main.34ee57e0.js"
+  },
+  {
+    "revision": "8203d7603463db4bdd8ab7e970c748d3",
+    "url": "/react-store/static/media/bg.8203d760.jpeg"
+  },
+  {
+    "revision": "1e9b5dff4cbb3f628b44d29037c3ce7d",
+    "url": "/react-store/index.html"
+  }
+];
